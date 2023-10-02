b8042ba80a8b4195f7f5bbd01de710714733b3ae
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-02 00:23:42 -0400

    Gallery fully done with overlay

--- 04-PankraFest/src/js/app.js
+++ 04-PankraFest/src/js/app.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    iniciarFechas();
-    iniciarApp();
+  iniciarFechas();
+  iniciarApp();
 });
 
 function iniciarApp() {
-
-    crearGaleria();
+  crearGaleria();
 }
 
 function iniciarFechas() {
@@ -38,16 +37,60 @@ function iniciarFechas() {
   }
 }
 
-function crearGaleria(){
-    const galeria = document.querySelector(".galeria-imagenes");
+function crearGaleria() {
+  const galeria = document.querySelector(".galeria-imagenes");
 
-    for(let i = 1; i<=12; i++){
-        const imagen = document.createElement("picture");
-        imagen.innerHTML = `
+  for (let i = 1; i <= 12; i++) {
+    const imagen = document.createElement("picture");
+    imagen.innerHTML = `
             <source srcset="build/img/thumb/${i}.avif" type="image/avif">
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
-            <img loading="lazy" width="50" height="50" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+            <img loading="lazy" width="25" height="25" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
         `;
-        galeria.appendChild(imagen);
-    }
+
+    imagen.onclick = () => {
+      mostrarImagen(i);
+    };
+    galeria.appendChild(imagen);
+  }
 }
+
+function mostrarImagen(id) {
+  const imagen = document.createElement("picture");
+  imagen.innerHTML = `
+            <source srcset="build/img/grande/${id}.avif" type="image/avif">
+            <source srcset="build/img/grande/${id}.webp" type="image/webp">
+            <img loading="lazy" width="25" height="25" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+        `;
+
+  //Se crea el Overlay
+
+  const overlay = document.createElement("div");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+
+  overlay.onclick = () => {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+
+  //Cerrar el modal
+
+  const cerrarModal = document.createElement("p");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+
+  cerrarModal.onclick = () => {
+    const body = document.querySelector("body");
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+
+  overlay.appendChild(cerrarModal);
+
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
+}
+
