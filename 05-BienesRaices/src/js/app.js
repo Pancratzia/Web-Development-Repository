2799816ee69fffc7598e1979da3399a48e024808
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-05 14:12:21 -0400

    Hamburger menu working

--- 05-BienesRaices/src/js/app.js
+++ 05-BienesRaices/src/js/app.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     fechas();
+    eventListeners();
 })
 
 function fechas(){
     const annosGeneradosEL = document.querySelectorAll(".annoGenerado");
     annosGeneradosEL.forEach(annoGenerado => {
         annoGenerado.innerHTML = new Date().getFullYear()
-    })
+    });
+}
 
-   
+function eventListeners(){
+    const mobileMenu = document.querySelector(".mobile-menu");
+    mobileMenu.addEventListener("click", navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector(".navegacion");
+    navegacion.classList.toggle("mostrar");
 }
