0d995d199ecbaf676659f0880dc488a78e4f8805
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-08 18:49:24 -0400

    Table to list properties fully done

--- 05-BienesRaices/admin/index.php
+++ 05-BienesRaices/admin/index.php
@@ -15,6 +15,32 @@
         ?>
 
         <a href="propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la Playa</td>
+                    <td><img src="../imagenes/412ca16791d6d005ba51a882e859d4e2.jpg" alt="Casa en la playa" class="imagen-tabla"></td>
+                    <td>300000$</td>
+                    <td>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
     </main>
 
 <?php
