d15cb98d2c52856532d63cdc2722ddbc2ab63ee0
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-08 10:04:10 -0400

    creating a template header

--- 05-BienesRaices/entrada.php
+++ 05-BienesRaices/entrada.php
@@ -1,40 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienes Raices</title>
 
-    <link rel="preload" href="build/css/app.css" as="style">
-    <link rel="stylesheet" href="build/css/app.css">
-</head>
-<body>
-
-    <header class="header">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="index.php">
-                    <img width="50" height="50" src="build/img/logo.svg" alt="Logo de Bienes Raices">
-                </a>
-
-                <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono menu responsive">
-                </div>
-
-                  <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="Dark mode icon">
-                    <nav class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
-                    </nav>
-                </div>
-            </div><!-- barra -->
-
-        </div>
-    </header>
-
+<?php
+    $inicio = false;
+    include "includes/templates/header.php";
+?>
 
     <main class="contenedor seccion">
         <h1>Consejos para Limpiar tu Piscina</h1>
