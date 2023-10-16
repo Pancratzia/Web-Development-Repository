64041a8702308ed6ab3302ee0b312b4a7d0d74ea
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-16 17:26:46 -0400

    Updating correctly

--- 06-BienesRaicesMVC/controllers/PropiedadController.php
+++ 06-BienesRaicesMVC/controllers/PropiedadController.php
@@ -71,6 +71,33 @@ class PropiedadController{
 
         $errores = Propiedad::getErrores();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $args = $_POST['propiedad'];
+        
+            $propiedad->sincronizar($args);
+        
+            $errores = $propiedad->validar();
+        
+            $extension = pathinfo($_FILES['propiedad']['name']['imagen'], PATHINFO_EXTENSION);
+            $nombreImagen = md5(uniqid(rand(), true)) . '.' . $extension;
+        
+            if ($_FILES['propiedad']['tmp_name']['imagen']) {
+                $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
+                $propiedad->setImagen($nombreImagen);
+            }
+        
+            if (empty($errores)) {
+        
+                if($_FILES['propiedad']['tmp_name']['imagen']) {
+                    $image->save(CARPETA_IMAGENES . $nombreImagen);
+                }
+        
+                $propiedad->guardar();
+            }
+        }
+        
+
         $router->render('propiedades/actualizar', [
             'propiedad' => $propiedad,
             'errores' => $errores,
