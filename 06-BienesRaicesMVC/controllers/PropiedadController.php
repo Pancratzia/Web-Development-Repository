73f58a2addc11e3a35c5d899e274e24c4f28d592
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-16 17:53:33 -0400

    create seller view started

--- 06-BienesRaicesMVC/controllers/PropiedadController.php
+++ 06-BienesRaicesMVC/controllers/PropiedadController.php
@@ -1,26 +1,32 @@
 <?php
 
 namespace Controllers;
+
 use MVC\Router;
 use Model\Propiedad;
 use Model\Vendedor;
 use Intervention\Image\ImageManagerStatic as Image;
 
-class PropiedadController{
+class PropiedadController
+{
 
-    public static function index(Router $router){
+    public static function index(Router $router)
+    {
 
         $propiedades = Propiedad::all();
+        $vendedores = Vendedor::all();
         $resultado = $_GET['resultado'] ?? null;
 
 
         $router->render('propiedades/admin', [
             'propiedades' => $propiedades,
-            'resultado' => $resultado
+            'resultado' => $resultado,
+            'vendedores' => $vendedores
         ]);
     }
 
-    public static function crear(Router $router){
+    public static function crear(Router $router)
+    {
 
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
@@ -30,31 +36,31 @@ class PropiedadController{
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $propiedad = new Propiedad($_POST['propiedad']);
-           
+
             $extension = pathinfo($_FILES['propiedad']['name']['imagen'], PATHINFO_EXTENSION);
-        
+
             $nombreImagen = md5(uniqid(rand(), true)) . '.' . $extension;
-        
+
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
                 $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
                 $propiedad->setImagen($nombreImagen);
             }
-        
+
             $errores = $propiedad->validar();
-        
-        
+
+
             if (empty($errores)) {
-        
+
                 if (!is_dir(CARPETA_IMAGENES)) {
                     mkdir(CARPETA_IMAGENES);
                 }
-        
+
                 $image->save(CARPETA_IMAGENES . $nombreImagen);
-        
+
                 $propiedad->guardar();
             }
         }
-        
+
 
         $router->render('propiedades/crear', [
             'propiedad' => $propiedad,
@@ -63,7 +69,8 @@ class PropiedadController{
         ]);
     }
 
-    public static function actualizar(Router $router){
+    public static function actualizar(Router $router)
+    {
         $id = validarORedireccionar('/admin');
 
         $propiedad = Propiedad::find($id);
@@ -74,29 +81,29 @@ class PropiedadController{
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             $args = $_POST['propiedad'];
-        
+
             $propiedad->sincronizar($args);
-        
+
             $errores = $propiedad->validar();
-        
+
             $extension = pathinfo($_FILES['propiedad']['name']['imagen'], PATHINFO_EXTENSION);
             $nombreImagen = md5(uniqid(rand(), true)) . '.' . $extension;
-        
+
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
                 $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
                 $propiedad->setImagen($nombreImagen);
             }
-        
+
             if (empty($errores)) {
-        
-                if($_FILES['propiedad']['tmp_name']['imagen']) {
+
+                if ($_FILES['propiedad']['tmp_name']['imagen']) {
                     $image->save(CARPETA_IMAGENES . $nombreImagen);
                 }
-        
+
                 $propiedad->guardar();
             }
         }
-        
+
 
         $router->render('propiedades/actualizar', [
             'propiedad' => $propiedad,
@@ -105,6 +112,25 @@ class PropiedadController{
         ]);
     }
 
+    public static function eliminar()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+
+            if ($id) {
+
+                $tipo = $_POST['tipo'];
+
+
+                if (validarTipoContenido($tipo)) {
+                    $propiedad = Propiedad::find($id);
+                    $propiedad->eliminar();
+                }
+            } else {
+                header('Location: ../admin');
+            }
+        }
+    }
 }
--- 06-BienesRaicesMVC/views/propiedades/admin.php
+++ 06-BienesRaicesMVC/views/propiedades/admin.php
@@ -36,9 +36,40 @@
                     <td><?php echo $propiedad->precio; ?>$</td>
                     <td>
                         <a href="propiedades/actualizar?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
-                        <form method="POST" class="w-100">
+                        <form method="POST" class="w-100" action="propiedades/eliminar">
                             <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
                             <input type="hidden" name="tipo" value="propiedad">
+                            <input type="submit" value="Eliminar" class="boton-rojo-block" />
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+    <h2>Vendedores</h2>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Teléfono</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php foreach ($vendedores as $vendedor) : ?>
+                <tr>
+                    <td><?php echo $vendedor->id; ?></td>
+                    <td><?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td><?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <a href="vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id; ?>">
+                            <input type="hidden" name="tipo" value="vendedor">
                             <input type="submit" value="Eliminar" class="boton-rojo-block" />
                         </form>
                     </td>
