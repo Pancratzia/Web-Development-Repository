adfc39395ffd9f29cf002f60827b99a86cdafb7b
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-20 11:04:58 -0400

    Creating form structure for login form

--- 07-AppSalon/views/auth/login.php
+++ 07-AppSalon/views/auth/login.php
@@ -1,2 +1,23 @@
 <h1 class="nombre-pagina">Login</h1>
 <p class="descripcion-pagina">Inicia Sesión con tus Datos</p>
+
+<form action="/" method="POST" class="formulatio">
+
+    <div class="campo">
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Tu Email">
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="Tu Password">
+    </div>
+
+    <input type="submit" value="Iniciar Sesión">
+
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Registrarse</a>
+    <a href="/olvide">Recuperar Contraseña</a>
+</div>
