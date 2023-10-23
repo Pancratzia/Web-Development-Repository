e1495c7cfcd9efe733277d79042bfe503f800911
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-23 19:42:31 -0400

    Tab finished

--- 07-AppSalon/src/js/app.js
+++ 07-AppSalon/src/js/app.js
@@ -1,30 +1,39 @@
 let paso = 1;
 
+document.addEventListener("DOMContentLoaded", () => {
+  iniciarApp();
+});
 
-document.addEventListener('DOMContentLoaded', () => {
-    iniciarApp();
-})
-
-function iniciarApp(){
-
-
-    tabs();
+function iniciarApp() {
+  mostrarSeccion();
+  tabs();
 }
 
+function tabs() {
+  const botones = document.querySelectorAll(".tabs button");
 
-function tabs(){
-
-    const botones = document.querySelectorAll('.tabs button');
-
-
-    botones.forEach( boton => {
-        boton.addEventListener('click', (e) => {
-            paso = parseInt(e.target.dataset.paso);
-            mostrarSeccion();
-        });
+  botones.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      paso = parseInt(e.target.dataset.paso);
+      mostrarSeccion();
     });
+  });
 }
 
-function mostrarSeccion(){
-    
+function mostrarSeccion() {
+  const seccionAnterior = document.querySelector(`.mostrar`);
+  if(seccionAnterior) {
+    seccionAnterior.classList.remove("mostrar");
+  }
+
+  const seccion = document.querySelector(`#paso-${paso}`);
+  seccion.classList.add("mostrar");
+
+  const tabAnterior = document.querySelector(".actual");
+  if(tabAnterior) {
+    tabAnterior.classList.remove("actual");
+  }
+
+  const tab = document.querySelector(`[data-paso="${paso}"]`);
+  tab.classList.add("actual");
 }
