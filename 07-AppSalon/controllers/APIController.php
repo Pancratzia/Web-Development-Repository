0d47532e976ed80fbda1f881f746a5bb04e4c7de
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-26 09:53:41 -0400

    saving information on bridge table

--- 07-AppSalon/controllers/APIController.php
+++ 07-AppSalon/controllers/APIController.php
@@ -2,6 +2,7 @@
 namespace Controllers;
 
 use Model\Cita;
+use Model\CitaServicio;
 use Model\Servicio;
 
 class APIController
@@ -17,10 +18,23 @@ class APIController
     public static function guardar(){
         
         $cita = new Cita($_POST);
-
         $resultado = $cita->guardar();
 
-        echo json_encode($resultado);
+        $id = $resultado['id'];
+
+        $idServicios = explode(",", $_POST['servicios']);
+
+        foreach ($idServicios as $idServicio) {
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+        
+        echo json_encode(['resultado' => $resultado]);
     }
 
 }
