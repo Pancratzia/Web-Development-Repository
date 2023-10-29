7fbf6b75d02119054b73fc091602d4af83589b50
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-29 18:11:06 -0400

    Creating login structure

--- 08-UpTask/views/auth/login.php
+++ 08-UpTask/views/auth/login.php
@@ -1,1 +1,27 @@
-<h1>Login</h1>
+<div class="contendor">
+    <h1>UpTask</h1>
+    <p>Crea y Administra tus Proyectos</p>
+
+    <div class="contenedor-sm">
+        <div class="descripcion-pagina">Iniciar Sesión</div>
+
+        <form class="formulario" action="/" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Tu Email">
+            </div>
+
+            <div class="campo">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Tu Password">
+            </div>
+
+            <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">Crear Cuenta en UpTask</a>
+            <a href="/olvide">¿Olvidaste tu Password"></a>
+        </div>
+    </div>
+</div>
