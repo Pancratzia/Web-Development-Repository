d1e602974ba9976e19a86b3e9b4e05ecca1a54c5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-30 22:45:30 -0400

    Creating dashboard structure

--- 08-UpTask/controllers/DashboardController.php
+++ 08-UpTask/controllers/DashboardController.php
@@ -6,8 +6,11 @@ use MVC\Router;
 
 class DashboardController {
     
-
     public static function index(Router $router) {
+
+        if(!isset($_SESSION)) {
+            session_start();
+        }
 
         $router->render('dashboard/index', [
             
