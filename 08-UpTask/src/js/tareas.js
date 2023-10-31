498501ec0dc4a65252ea0305b311d798726ffb98
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-31 12:03:34 -0400

    Starting modal construction

--- 08-UpTask/src/js/tareas.js
+++ 08-UpTask/src/js/tareas.js
@@ -1,10 +1,33 @@
-(function(){
+(function () {
+  const nuevaTareaBtn = document.querySelector("#agregar-tarea");
+  nuevaTareaBtn.addEventListener("click", mostrarFormulario);
 
-    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
-    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+  function mostrarFormulario(e) {
+    const modal = document.createElement("DIV");
+    modal.classList.add("modal");
 
-    function mostrarFormulario(e){
-        alert('hola');
-    }
+    modal.innerHTML = `
 
+            <form class="formulario nueva-tarea">
+                <legend>Agrega una Nueva Tarea</legend>
+
+                <div class="campo">
+                    <label for="tarea">Nombre de la Tarea</label>
+                    <input type="text" id="tarea" name="tarea" placeholder="Nombre de la Tarea">
+                </div>
+
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Agregar Tarea" />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+    `;
+
+    setTimeout(() => {
+        const formulario = document.querySelector(".formulario");
+        formulario.classList.add("animar");
+    }, 3000);
+
+    document.querySelector("body").appendChild(modal);
+  }
 })();
