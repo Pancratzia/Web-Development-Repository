a41f82172cea5b3aab3b7bda25b9d621d97b54dd
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-10-31 12:56:52 -0400

    Adding new tables

--- 08-UpTask/uptask_mvc.sql
+++ 08-UpTask/uptask_mvc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 30-10-2023 a las 16:55:29
+-- Tiempo de generación: 31-10-2023 a las 16:56:27
 -- Versión del servidor: 8.0.30
 -- Versión de PHP: 8.1.10
 
@@ -24,6 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `proyectos`
+--
+
+CREATE TABLE `proyectos` (
+  `id` int NOT NULL,
+  `proyecto` varchar(100) COLLATE utf16_bin NOT NULL,
+  `url` varchar(32) COLLATE utf16_bin NOT NULL,
+  `propietarioid` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Volcado de datos para la tabla `proyectos`
+--
+
+INSERT INTO `proyectos` (`id`, `proyecto`, `url`, `propietarioid`) VALUES
+(1, ' Tienda Virtual Para Camisetas', '56a55b3487305480cb9028361c308191', 1),
+(2, ' Blog de Perros', 'a7deca2510e5d0dd51375510eb7b0153', 1),
+(3, ' Tienda Virtual', 'd98d36496551f4821df093585f2289b5', 2),
+(4, ' Visitar a Tiffany', '233c09488a673b493a85bc1bdaaeeb9f', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tareas`
+--
+
+CREATE TABLE `tareas` (
+  `id` int NOT NULL,
+  `nombre` varchar(60) COLLATE utf16_bin NOT NULL,
+  `estado` tinyint(1) NOT NULL,
+  `proyectoid` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
@@ -32,13 +68,35 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(30) COLLATE utf16_bin NOT NULL,
   `email` varchar(255) COLLATE utf16_bin NOT NULL,
   `password` varchar(60) COLLATE utf16_bin NOT NULL,
-  `token` varchar(15) COLLATE utf16_bin NOT NULL,
+  `token` varchar(32) CHARACTER SET utf16 COLLATE utf16_bin NOT NULL,
   `confirmado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `token`, `confirmado`) VALUES
+(1, ' User Admin', 'test@admin.com', '$2y$10$gckZetYSjUWNH5V/0Ukca.xHbzIYCYoXNTpN9eArdRxNXK7a9FAVK', '', 1),
+(2, ' Pancratzia', 'correo@correo.com', '$2y$10$dN5emssE/L8yRzqxYVuuauhZDJv9GzqNBrSrexTQZ4VdeXPB20kdy', '', 1);
+
+--
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `propietarioid` (`propietarioid`);
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `proyectoid` (`proyectoid`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -51,10 +109,32 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `proyectos`
+--
+ALTER TABLE `proyectos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD CONSTRAINT `tareas_ibfk_1` FOREIGN KEY (`proyectoid`) REFERENCES `proyectos` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
