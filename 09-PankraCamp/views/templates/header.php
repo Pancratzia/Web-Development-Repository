f41d2ed1080296771aa57b41eefda21ebe0c4bf8
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-03 07:37:18 -0400

    Creating structure for header template

---
+++ 09-PankraCamp/views/templates/header.php
@@ -0,0 +1,21 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar Sesion</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="/">
+                <h1 class="header__logo">&#60;PankraCamp/></h1>
+            </a>
+
+            <p class="header__texto">
+                Septiembre 1-2 del <?php echo date('Y')+1; ?>
+            </p>
+            <p class="header__texto header__texto--modalidad">En línea - Presencial</p>
+
+            <a href="/registro" class="header__boton">Comprar Pase</a>
+        </div>
+    </div>
+</header>
