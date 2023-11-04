cf777c4571f44390f334d439970dd0ac2191aa35
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-04 10:13:16 -0400

    Creating /message page

---
+++ 09-PankraCamp/views/auth/mensaje.php
@@ -0,0 +1,6 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">
+        Revisa tu bandeja de correos y sigue los pasos para activar tu cuenta en PankraCamp
+    </p>
+</main>
