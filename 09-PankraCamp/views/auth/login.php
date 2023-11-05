56236b7ec10781f8bfd7eecbb5248afc4b06317f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-04 22:08:11 -0400

    Login fully done

--- 09-PankraCamp/views/auth/login.php
+++ 09-PankraCamp/views/auth/login.php
@@ -2,15 +2,17 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Inicia Sesión en PankraCamp</p>
 
-    <form action="" class="formulario">
+    <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
+
+    <form method="POST" action="/login" class="formulario">
         <div class="formulario__campo">
             <label for="email" class="formulario__label">Email</label>
-            <input type="email" class="formulario__input" placeholder="Tu Email..." id="email">
+            <input type="email" class="formulario__input" placeholder="Tu Email..." id="email" name="email">
         </div>
 
         <div class="formulario__campo">
             <label for="password" class="formulario__label">Password</label>
-            <input type="password" class="formulario__input" placeholder="Tu Password..." id="password">
+            <input type="password" class="formulario__input" placeholder="Tu Password..." id="password" name="password">
         </div>
 
         <input type="submit" value="Iniciar Sesión" class="formulario__submit">
