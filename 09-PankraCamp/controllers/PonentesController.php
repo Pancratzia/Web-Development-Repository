e49e8e112db10f4440385c1456aa730df6b39a20
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-06 11:28:13 -0400

    Updating fully done

--- 09-PankraCamp/controllers/PonentesController.php
+++ 09-PankraCamp/controllers/PonentesController.php
@@ -6,10 +6,12 @@ use Model\Ponente;
 use MVC\Router;
 use Intervention\Image\ImageManagerStatic as Image;
 
-class PonentesController {
-    
+class PonentesController
+{
 
-    public static function index(Router $router){
+
+    public static function index(Router $router)
+    {
 
         $ponentes = Ponente::all();
 
@@ -20,23 +22,24 @@ class PonentesController {
         ]);
     }
 
-    public static function crear(Router $router){
-        
+    public static function crear(Router $router)
+    {
+
         $alertas = [];
         $ponente = new Ponente;
 
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-            if(!empty($_FILES['imagen']['tmp_name'])){
+
+            if (!empty($_FILES['imagen']['tmp_name'])) {
                 $carpeta_imagenes = '../public/img/speakers';
-                if(!is_dir($carpeta_imagenes)){
+                if (!is_dir($carpeta_imagenes)) {
                     mkdir($carpeta_imagenes, 0755, true);
                 }
 
-                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('png', 80);
-                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('webp', 80);
-                $imagen_avif = Image::make($_FILES['imagen']['tmp_name'])->fit(800,800)->encode('avif', 80);
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+                $imagen_avif = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('avif', 80);
 
                 $nombre_imagen = md5(uniqid(rand(), true));
 
@@ -49,14 +52,14 @@ class PonentesController {
 
             $alertas = $ponente->validar();
 
-            if(empty($alertas)){
+            if (empty($alertas)) {
                 $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
                 $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.webp');
                 $imagen_avif->save($carpeta_imagenes . '/' . $nombre_imagen . '.avif');
 
                 $resultado = $ponente->guardar();
 
-                if($resultado){  
+                if ($resultado) {
                     header('Location: /admin/ponentes');
                 }
             }
@@ -70,20 +73,21 @@ class PonentesController {
         ]);
     }
 
-    public static function editar(Router $router){
-        
+    public static function editar(Router $router)
+    {
+
         $alertas = [];
 
         $id = $_GET['id'];
         $id = filter_var($id, FILTER_VALIDATE_INT);
 
-        if(!$id){
+        if (!$id) {
             header('Location: /admin/ponentes');
         }
 
         $ponente = Ponente::find($id);
 
-        if(empty($ponente)){
+        if (empty($ponente)) {
             header('Location: /admin/ponentes');
         }
 
@@ -91,12 +95,52 @@ class PonentesController {
 
         $redes = json_decode($ponente->redes);
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+            if (!empty($_FILES['imagen']['tmp_name'])) {
+
+                $carpeta_imagenes = '../public/img/speakers';
+                if (!is_dir($carpeta_imagenes)) {
+                    mkdir($carpeta_imagenes, 0755, true);
+                }
+
+                $imagen_png = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('png', 80);
+                $imagen_webp = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('webp', 80);
+                $imagen_avif = Image::make($_FILES['imagen']['tmp_name'])->fit(800, 800)->encode('avif', 80);
+
+                $nombre_imagen = md5(uniqid(rand(), true));
+
+                $_POST['imagen'] = $nombre_imagen;
+            } else {
+                $_POST['imagen'] = $ponente->imagen_actual;
+            }
+
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+            $ponente->sincronizar($_POST);
+            $alertas = $ponente->validar();
+
+            if (empty($alertas)) {
+
+                if (isset($nombre_imagen)) {
+                    $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+                    $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.webp');
+                    $imagen_avif->save($carpeta_imagenes . '/' . $nombre_imagen . '.avif');
+                }
+
+                $resultado = $ponente->guardar();
+
+                if($resultado) {
+                    header('Location: /admin/ponentes');
+                }
+            }
+        }
+
         $router->render('admin/ponentes/editar', [
             'titulo' => 'Actualizar Ponente / Conferencista',
             'alertas' => $alertas,
             'ponente' => $ponente,
             'redes' => $redes
         ]);
-
     }
 }
