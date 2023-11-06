51c72cf2ac3deb2371616a305976997306c91670
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-05 23:41:49 -0400

    Deleting tag on double click

--- 09-PankraCamp/src/js/tags.js
+++ 09-PankraCamp/src/js/tags.js
@@ -4,6 +4,9 @@
 
     if( tagsInput ){
 
+        const tagsDiv = document.getElementById('tags');
+        const tagsInputHidden = document.querySelector('[name="tags"]');
+
         let tags = [];
         
         tagsInput.addEventListener('keypress', guardarTag);
@@ -19,9 +22,40 @@
                 e.preventDefault();
                 tags = [...tags, e.target.value.trim()];
                 tagsInput.value = '';
+
+                mostrarTags();
             }
 
         }
+
+        function mostrarTags(){
+            tagsDiv.textContent = '';
+
+            tags.forEach( tag => {
+                const etiqueta = document.createElement('LI');
+                etiqueta.classList.add('formulario__tag');
+                etiqueta.textContent = tag;
+
+                etiqueta.ondblclick = eliminarTag;
+
+                tagsDiv.appendChild(etiqueta);
+
+
+            });
+
+            actualizarInputHidden();
+        }
+
+        function eliminarTag(e){
+            e.target.remove();
+            tags = tags.filter( tag => tag !== e.target.textContent );
+            actualizarInputHidden();
+        }
+
+        function actualizarInputHidden(){
+            tagsInputHidden.value = tags.toString();
+        }
+  
     }
 
 }());
