7b9bfba256514bcfbe74e85e955484bfab23fee6
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-06 00:15:10 -0400

    Finishing with create and upload images

--- 09-PankraCamp/controllers/PonentesController.php
+++ 09-PankraCamp/controllers/PonentesController.php
@@ -40,8 +40,23 @@ class PonentesController {
                 $_POST['imagen'] = $nombre_imagen;
             }
 
+            $_POST['redes'] = json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+
             $ponente->sincronizar($_POST);
+
             $alertas = $ponente->validar();
+
+            if(empty($alertas)){
+                $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . '.png');
+                $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . '.webp');
+                $imagen_avif->save($carpeta_imagenes . '/' . $nombre_imagen . '.avif');
+
+                $resultado = $ponente->guardar();
+
+                if($resultado){  
+                    header('Location: /admin/ponentes');
+                }
+            }
         }
 
         $router->render('admin/ponentes/crear', [
