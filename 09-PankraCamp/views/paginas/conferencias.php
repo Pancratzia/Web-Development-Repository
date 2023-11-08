82709ac79abd7461ddf7f02d8c47175de0ee1160
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-08 17:10:55 -0400

    Showing all conferences and workshops

--- 09-PankraCamp/views/paginas/conferencias.php
+++ 09-PankraCamp/views/paginas/conferencias.php
@@ -11,42 +11,24 @@
         <div class="eventos__listado slider swiper">
             <div class="swiper-wrapper">
                 <?php foreach ($eventos['conferencias_1'] as $evento) : ?>
-                    <div class="evento swiper-slide">
-                        <p class="evento__hora">
-                            <?php echo $evento->hora->hora ?>
-                        </p>
-
-                        <div class="evento__informacion">
-                            <h4 class="evento__nombre"> <?php echo $evento->nombre ?> </h4>
-
-                            <p class="evento__introduccion""><?php echo $evento->descripcion ?></p>
-
-                        <div class=" evento__autor-info">
-                                <picture>
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen . '.avif'; ?>" type="image/avif">
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen . '.webp'; ?>" type="image/webp">
-                                    <source srcset="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen . '.jpg'; ?>" type="image/png">
-
-                                    <img loading="lazy" width="200" height="300" class="evento__imagen-autor" src="<?php echo $_ENV['HOST'] . '/img/speakers/' . $evento->ponente->imagen; ?>.png" alt="Imagen del Ponente <?php echo $evento->ponente->nombre; ?>">
-
-                                </picture>
-
-                            <p class="evento__autor-nombre">
-                                <?php echo $evento->ponente->nombre . ' ' . $evento->ponente->apellido; ?>
-                            </p>
-                        </div>
-
-                    </div>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php endforeach ?>
             </div>
-        <?php endforeach; ?>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-    </div>
 
-    <p class="eventos__fecha">Miércoles 18 de Septiembre</p>
+        <p class="eventos__fecha">Miércoles 18 de Septiembre</p>
 
-    <div class="eventos__listado"></div>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['conferencias_2'] as $evento) : ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php endforeach ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
 
     </div>
 
@@ -55,11 +37,27 @@
         <h3 class="eventos__heading">&lt; Workshops /&gt;</h3>
         <p class="eventos__fecha">Martes 17 de Septiembre</p>
 
-        <div class="eventos__listado"></div>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['workshops_1'] as $evento) : ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php endforeach ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
 
         <p class="eventos__fecha">Miércoles 18 de Septiembre</p>
 
-        <div class="eventos__listado"></div>
+        <div class="eventos__listado slider swiper">
+            <div class="swiper-wrapper">
+                <?php foreach ($eventos['workshops_2'] as $evento) : ?>
+                    <?php include __DIR__ . '../../templates/evento.php'; ?>
+                <?php endforeach ?>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
 
     </div>
 
