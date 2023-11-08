0c631f88140e4f6e1a6ea170899d726c61356b82
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-08 10:28:28 -0400

    Creating initial styles for workshops and conferences page

---
+++ 09-PankraCamp/views/paginas/conferencias.php
@@ -0,0 +1,32 @@
+<main class="agenda">
+
+    <h2 class="agenda__heading"><?php echo $titulo ?></h2>
+    <p class="agenda__descripcion">Talleres y Conferencias Dictadas por Expertos de todo el mundo</p>
+
+    <div class="eventos">
+
+        <h3 class="eventos__heading">&lt; Conferencias /&gt;</h3>
+        <p class="eventos__fecha">Martes 17 de Septiembre</p>
+
+        <div class="eventos__listado"></div>
+
+        <p class="eventos__fecha">Miércoles 18 de Septiembre</p>
+
+        <div class="eventos__listado"></div>
+
+    </div>
+
+    <div class="eventos eventos--workshops">
+        
+        <h3 class="eventos__heading">&lt; Workshops /&gt;</h3>
+        <p class="eventos__fecha">Martes 17 de Septiembre</p>
+
+        <div class="eventos__listado"></div>
+
+        <p class="eventos__fecha">Miércoles 18 de Septiembre</p>
+
+        <div class="eventos__listado"></div>
+
+    </div>
+
+</main>
