35095d82592508e1342df0e8beb8445b202494eb
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-08 00:14:37 -0400

    Packages fully done

---
+++ 09-PankraCamp/views/paginas/paquetes.php
@@ -0,0 +1,44 @@
+<main class="paquetes">
+    <h2 class="paquetes__heading"><?php echo $titulo ?></h2>
+    <p class="paquetes__descripcion">Conoce los paquetes de PankraCamp</p>
+
+    <div class="paquetes__grid">
+
+        <div class="paquete">
+            <h3 class="paquete__nombre">Starter</h3>
+            <ul class="paquete__lista">
+                <li class="paquete__elemento">Acceso Virtual a las Conferencias de PankraCamp</li>
+            </ul>
+
+            <p class="paquete__precio">0$</p>
+        </div>
+
+        <div class="paquete">
+            <h3 class="paquete__nombre">Platinum</h3>
+            <ul class="paquete__lista">
+                <li class="paquete__elemento">Acceso Presencial a PankraCamp</li>
+                <li class="paquete__elemento">Pase por 2 días</li>
+                <li class="paquete__elemento">Acceso a Talleres y Conferencias</li>
+                <li class="paquete__elemento">Acceso a las Grabaciones</li>
+                <li class="paquete__elemento">Camisa del Evento</li>
+                <li class="paquete__elemento">Comida y Bebida</li>
+            </ul>
+
+            <p class="paquete__precio">100$</p>
+        </div>
+
+        <div class="paquete">
+            <h3 class="paquete__nombre">Gold</h3>
+            <ul class="paquete__lista">
+                <li class="paquete__elemento">Acceso Virtual a PankraCamp</li>
+                <li class="paquete__elemento">Pase por 2 días</li>
+                <li class="paquete__elemento">Enlace a Talleres y Conferencias</li>
+                <li class="paquete__elemento">Acceso a las Grabaciones</li>
+            </ul>
+
+            <p class="paquete__precio">50$</p>
+
+        </div>
+
+    </div>
+</main>
