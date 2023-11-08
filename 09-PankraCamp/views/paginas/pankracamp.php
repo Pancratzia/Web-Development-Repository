7aedcba463841e8206a40d156c0a045ae8f87010
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-07 23:57:50 -0400

    Pankracamp page created (About Pankracamp)

---
+++ 09-PankraCamp/views/paginas/pankracamp.php
@@ -0,0 +1,20 @@
+<main class="pankracamp">
+    <h2 class="pankracamp__heading"><?php echo $titulo; ?></h2>
+    <p class="pankracamp__descripcion">Conoce la Conferencia más Importante de Latinoamérica</p>
+
+    <div class="pankracamp__grid">
+        <picture class="pankracamp__imagen">
+            <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+            <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+            <source srcset="build/img/sobre_devwebcamp.jpg" type="image/jpeg">
+            <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="imagen conferencia PankraCamp">
+        </picture>
+
+        <div class="pankracamp__contenido">
+            <p class="pankracamp__texto">Non cupiditate doloribus quo earum nemo dolorem illum quae laboriosam voluptas, suscipit, officia repudiandae eveniet ea odio cumque possimus. Pariatur, sunt ullam molestiae velit porro aut rem quo, quasi nam, ipsam aperiam? Et quod laborum accusamus consequatur! Omnis esse libero a perspiciatis!</p>
+            <p class="pankracamp__texto">Assumenda, dignissimos porro. Qui, veritatis? Magnam reprehenderit distinctio quia? Consequuntur sequi ipsum repellendus tempora? Deserunt saepe rerum, at, beatae ullam vitae excepturi repudiandae commodi ad, ipsam vero minima voluptate sit eligendi natus accusamus dolorum deleniti hic optio? Omnis, impedit accusamus.</p>
+        </div>
+
+    </div>
+
+</main>
