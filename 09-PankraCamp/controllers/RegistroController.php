3e6f68cee1bf3a5ade855412e41023865a716bb3
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-11 14:01:17 -0400

    Creating structure for selecting events and workshops page

--- 09-PankraCamp/controllers/RegistroController.php
+++ 09-PankraCamp/controllers/RegistroController.php
@@ -2,9 +2,14 @@
 
 namespace Controllers;
 
+use Model\Categoria;
+use Model\Dia;
+use Model\Evento;
+use Model\Hora;
 use Model\Registro;
 use Model\Usuario;
 use Model\Paquete;
+use Model\Ponente;
 use MVC\Router;
 
 class RegistroController
@@ -116,6 +121,57 @@ class RegistroController
         $router->render('registro/boleto', [
             'titulo' => 'Asistencia a PankraCamp',
             'registro' => $registro
+        ]);
+    }
+
+    public static function conferencias(Router $router)
+    {
+
+
+        if(!is_auth()){
+            header('Location: /login');
+            return;
+        }
+
+        $usuario_id = $_SESSION['id'];
+        $registro = Registro::where('usuario_id', $usuario_id);
+
+        if($registro->paquete_id !== "1"){
+            header('Location: /');
+            return;
+        }
+
+        $eventos = Evento::ordenar('hora_id', 'ASC');
+        $eventosFormateados = [];
+        
+        foreach ($eventos as $evento) {
+
+            $evento->categoria = Categoria::find($evento->categoria_id);
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+            
+            if($evento->dia_id === '1' && $evento->categoria_id === '1'){
+                $eventosFormateados['conferencias_1'][] = $evento;
+            }
+
+            if($evento->dia_id === '2' && $evento->categoria_id === '1'){
+                $eventosFormateados['conferencias_2'][] = $evento;
+            }
+
+            if($evento->dia_id === '1' && $evento->categoria_id === '2'){
+                $eventosFormateados['workshops_1'][] = $evento;
+            }
+
+            if($evento->dia_id === '2' && $evento->categoria_id === '2'){
+                $eventosFormateados['workshops_2'][] = $evento;
+            }
+
+        }
+
+        $router->render('registro/conferencias', [
+            'titulo' => 'Elige tus Workshops y Conferencias',
+            'eventos' => $eventosFormateados
         ]);
     }
 }
---
+++ 09-PankraCamp/views/registro/conferencias.php
@@ -0,0 +1,19 @@
+<h2 class="pagina__heading"><?php echo $titulo ?></h2>
+<p class="pagina__descripcion">Elige hasta 5 Workshops o Conferencias para asistir de forma presencial</p>
+
+<div class="eventos-registro">
+    <main class="eventos-registro__listado">
+        <h3 class="eventos-registro__heading--conferencias">&lt; Conferencias /&gt;</h3>
+        <p class="eventos-registro__fecha">Martes 17 de Septiembre</p>
+
+        <div class="eventos-registro__grid">
+            <?php foreach ($eventos['conferencias_1'] as $evento) : ?>
+                <?php include __DIR__ . '/evento.php'; ?>
+            <?php endforeach ?>
+        </div>
+    </main>
+
+    <aside>
+
+    </aside>
+</div>
