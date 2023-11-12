508f109221d3a643f459e2be0871c7866baffd8a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-12 16:21:43 -0400

    Finishing last project. Modules to be tested and deployed

--- 09-PankraCamp/models/ActiveRecord.php
+++ 09-PankraCamp/models/ActiveRecord.php
@@ -225,4 +225,21 @@ class ActiveRecord {
 
         return array_shift($total);
     }
+
+    public static function totalArray($array = [] ){
+        $query = "SELECT COUNT(*) FROM " . static::$tabla . " WHERE ";
+
+        foreach($array as $key => $value) {
+            if($key === array_key_last($array)) {
+                $query .= " $key = '$value'";
+            } else {
+                $query .= " $key = '$value' AND ";
+            }
+        }
+
+        $resultado = self::$db->query($query);
+        $total = $resultado->fetch_array();
+
+        return array_shift($total);
+    }
 }
--- 09-PankraCamp/src/js/regalos.js
+++ 09-PankraCamp/src/js/regalos.js
@@ -2,41 +2,50 @@
   const ctx = document.getElementById("myChart");
 
   if (ctx) {
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              "#ea580c",
-              "#84cc16",
-              "#22d3ee",
-              "#a855f7",
-              "#ef4444",
-              "#14b8a6",
-              "#db2777",
-              "#e11d48",
-              "#7e22ce",
-            ],
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+
+    obtenerDatos();
+    async function obtenerDatos() {
+      const url = "/api/regalos";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: resultado.map((regalo) => regalo.nombre),
+          datasets: [
+            {
+              label: "# of Votes",
+              data: resultado.map((regalo) => regalo.total),
+              backgroundColor: [
+                "#ea580c",
+                "#84cc16",
+                "#22d3ee",
+                "#a855f7",
+                "#ef4444",
+                "#14b8a6",
+                "#db2777",
+                "#e11d48",
+                "#7e22ce",
+              ],
+            },
+          ],
         },
-        plugins:{
-          legend: {
-            display: false
-          }
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
+          },
+          plugins:{
+            legend: {
+              display: false
+            }
+          },
+          responsive: true
         },
-        responsive: true
-      },
-    });
+      });
+
+    }
   }
 })();
