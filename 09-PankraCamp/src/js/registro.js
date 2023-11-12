fd7962e168977999d38572f5fb8c686e343f2ebc
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-12 10:28:49 -0400

    Deleting events from array and limiting events to 5 per person

--- 09-PankraCamp/src/js/registro.js
+++ 09-PankraCamp/src/js/registro.js
@@ -1,58 +1,68 @@
-(function(){
-    let eventos = [];
-    const resumen = document.querySelector("#registro-resumen");
-    const eventosBoton = document.querySelectorAll(".evento__agregar");
+(function () {
+  let eventos = [];
+  const resumen = document.querySelector("#registro-resumen");
+  const eventosBoton = document.querySelectorAll(".evento__agregar");
 
-    eventosBoton.forEach((boton) => {
-        boton.addEventListener("click", seleccionarEvento);
-    });
+  eventosBoton.forEach((boton) => {
+    boton.addEventListener("click", seleccionarEvento);
+  });
 
-    function seleccionarEvento({ target }){
+  function seleccionarEvento({ target }) {
+    if (eventos.length < 5) {
+      target.disabled = true;
 
-        target.disabled = true;
+      eventos = [
+        ...eventos,
+        {
+          id: target.dataset.id,
+          titulo: target.parentElement
+            .querySelector(".evento__nombre")
+            .textContent.trim(),
+        },
+      ];
 
-        eventos = [...eventos, {
-            id: target.dataset.id,
-            titulo: target.parentElement.querySelector(".evento__nombre").textContent.trim(),
-        }];
-
-        mostrarEventos();
+      mostrarEventos();
+    }else{
+        alert("Ya no puedes agregar más eventos");
     }
+  }
 
-    function mostrarEventos(){
+  function mostrarEventos() {
+    limpiarEventos();
 
-        limpiarEventos();
+    if (eventos.length > 0) {
+      eventos.forEach((evento) => {
+        const eventoDOM = document.createElement("DIV");
+        eventoDOM.classList.add("registro__evento");
 
-        if(eventos.length > 0){
-            eventos.forEach((evento) => {
-               const eventoDOM = document.createElement("DIV");
-               eventoDOM.classList.add("registro__evento"); 
+        const titulo = document.createElement("H3");
+        titulo.classList.add("registro__nombre");
+        titulo.textContent = evento.titulo;
 
-               const titulo = document.createElement("H3");
-               titulo.classList.add("registro__nombre");
-               titulo.textContent = evento.titulo;
+        const botonEliminar = document.createElement("BUTTON");
+        botonEliminar.classList.add("registro__eliminar");
+        botonEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        botonEliminar.onclick = () => {
+          eliminarEvento(evento.id);
+        };
 
-               const botonEliminar = document.createElement("BUTTON");
-               botonEliminar.classList.add("registro__eliminar");
-               botonEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>';
-               botonEliminar.onclick = () => {
-                   eliminarEvento(evento.id);
-               }
-
-               eventoDOM.appendChild(titulo);
-               eventoDOM.appendChild(botonEliminar);
-               resumen.appendChild(eventoDOM);
-            });
-        }
+        eventoDOM.appendChild(titulo);
+        eventoDOM.appendChild(botonEliminar);
+        resumen.appendChild(eventoDOM);
+      });
     }
+  }
 
-    function eliminarEvento(id){
-        alert("Eliminando evento... " + id);
-    }
+  function eliminarEvento(id) {
+    eventos = eventos.filter((event) => event.id !== id);
+    const botonAgregar = document.querySelector(`[data-id="${id}"]`);
+    botonAgregar.disabled = false;
+    mostrarEventos();
+  }
 
-    function limpiarEventos(){
-        while(resumen.firstChild){
-            resumen.removeChild(resumen.firstChild);
-        }
+  function limpiarEventos() {
+    while (resumen.firstChild) {
+      resumen.removeChild(resumen.firstChild);
     }
+  }
 })();
