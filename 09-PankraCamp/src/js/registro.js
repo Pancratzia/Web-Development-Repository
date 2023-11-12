892009ce2874180e6367bfa501cd9c6da5fffea4
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-12 10:25:03 -0400

    Starting with delete events inside list array

--- 09-PankraCamp/src/js/registro.js
+++ 09-PankraCamp/src/js/registro.js
@@ -1,5 +1,6 @@
 (function(){
     let eventos = [];
+    const resumen = document.querySelector("#registro-resumen");
     const eventosBoton = document.querySelectorAll(".evento__agregar");
 
     eventosBoton.forEach((boton) => {
@@ -8,11 +9,50 @@
 
     function seleccionarEvento({ target }){
 
+        target.disabled = true;
+
         eventos = [...eventos, {
             id: target.dataset.id,
             titulo: target.parentElement.querySelector(".evento__nombre").textContent.trim(),
         }];
 
-        target.disabled = true;
+        mostrarEventos();
+    }
+
+    function mostrarEventos(){
+
+        limpiarEventos();
+
+        if(eventos.length > 0){
+            eventos.forEach((evento) => {
+               const eventoDOM = document.createElement("DIV");
+               eventoDOM.classList.add("registro__evento"); 
+
+               const titulo = document.createElement("H3");
+               titulo.classList.add("registro__nombre");
+               titulo.textContent = evento.titulo;
+
+               const botonEliminar = document.createElement("BUTTON");
+               botonEliminar.classList.add("registro__eliminar");
+               botonEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+               botonEliminar.onclick = () => {
+                   eliminarEvento(evento.id);
+               }
+
+               eventoDOM.appendChild(titulo);
+               eventoDOM.appendChild(botonEliminar);
+               resumen.appendChild(eventoDOM);
+            });
+        }
+    }
+
+    function eliminarEvento(id){
+        alert("Eliminando evento... " + id);
+    }
+
+    function limpiarEventos(){
+        while(resumen.firstChild){
+            resumen.removeChild(resumen.firstChild);
+        }
     }
 })();
