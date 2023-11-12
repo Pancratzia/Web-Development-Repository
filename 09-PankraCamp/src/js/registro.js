eda88c0c72f417810061af8353cd0e0273c1a9a5
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-11-12 11:30:19 -0400

    Validating form submit

--- 09-PankraCamp/src/js/registro.js
+++ 09-PankraCamp/src/js/registro.js
@@ -3,73 +3,98 @@ import Swal from "sweetalert2";
 (function () {
   let eventos = [];
   const resumen = document.querySelector("#registro-resumen");
-  const eventosBoton = document.querySelectorAll(".evento__agregar");
 
-  eventosBoton.forEach((boton) => {
-    boton.addEventListener("click", seleccionarEvento);
-  });
+  if (resumen) {
+    const eventosBoton = document.querySelectorAll(".evento__agregar");
 
-  function seleccionarEvento({ target }) {
-    if (eventos.length < 5) {
-      target.disabled = true;
+    eventosBoton.forEach((boton) => {
+      boton.addEventListener("click", seleccionarEvento);
+    });
 
-      eventos = [
-        ...eventos,
-        {
-          id: target.dataset.id,
-          titulo: target.parentElement
-            .querySelector(".evento__nombre")
-            .textContent.trim(),
-        },
-      ];
+    const formularioRegistro = document.querySelector("#registro");
+    formularioRegistro.addEventListener("submit", submitFormulario);
 
-      mostrarEventos();
-    }else{
+    function seleccionarEvento({ target }) {
+      if (eventos.length < 5) {
+        target.disabled = true;
+
+        eventos = [
+          ...eventos,
+          {
+            id: target.dataset.id,
+            titulo: target.parentElement
+              .querySelector(".evento__nombre")
+              .textContent.trim(),
+          },
+        ];
+
+        mostrarEventos();
+      } else {
         Swal.fire({
-            title: "Error",
-            text: "No puedes agregar más de 5 eventos",
-            icon: "error",
-            confirmButtonText: "Ok",
+          title: "Error",
+          text: "No puedes agregar más de 5 eventos",
+          icon: "error",
+          confirmButtonText: "Ok",
         });
+      }
     }
-  }
 
-  function mostrarEventos() {
-    limpiarEventos();
+    function mostrarEventos() {
+      limpiarEventos();
 
-    if (eventos.length > 0) {
-      eventos.forEach((evento) => {
-        const eventoDOM = document.createElement("DIV");
-        eventoDOM.classList.add("registro__evento");
+      if (eventos.length > 0) {
+        eventos.forEach((evento) => {
+          const eventoDOM = document.createElement("DIV");
+          eventoDOM.classList.add("registro__evento");
 
-        const titulo = document.createElement("H3");
-        titulo.classList.add("registro__nombre");
-        titulo.textContent = evento.titulo;
+          const titulo = document.createElement("H3");
+          titulo.classList.add("registro__nombre");
+          titulo.textContent = evento.titulo;
 
-        const botonEliminar = document.createElement("BUTTON");
-        botonEliminar.classList.add("registro__eliminar");
-        botonEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>';
-        botonEliminar.onclick = () => {
-          eliminarEvento(evento.id);
-        };
+          const botonEliminar = document.createElement("BUTTON");
+          botonEliminar.classList.add("registro__eliminar");
+          botonEliminar.innerHTML = '<i class="fa-solid fa-trash"></i>';
+          botonEliminar.onclick = () => {
+            eliminarEvento(evento.id);
+          };
 
-        eventoDOM.appendChild(titulo);
-        eventoDOM.appendChild(botonEliminar);
-        resumen.appendChild(eventoDOM);
-      });
+          eventoDOM.appendChild(titulo);
+          eventoDOM.appendChild(botonEliminar);
+          resumen.appendChild(eventoDOM);
+        });
+      }
     }
-  }
 
-  function eliminarEvento(id) {
-    eventos = eventos.filter((event) => event.id !== id);
-    const botonAgregar = document.querySelector(`[data-id="${id}"]`);
-    botonAgregar.disabled = false;
-    mostrarEventos();
-  }
+    function eliminarEvento(id) {
+      eventos = eventos.filter((event) => event.id !== id);
+      const botonAgregar = document.querySelector(`[data-id="${id}"]`);
+      botonAgregar.disabled = false;
+      mostrarEventos();
+    }
 
-  function limpiarEventos() {
-    while (resumen.firstChild) {
-      resumen.removeChild(resumen.firstChild);
+    function limpiarEventos() {
+      while (resumen.firstChild) {
+        resumen.removeChild(resumen.firstChild);
+      }
+    }
+
+    function submitFormulario(e) {
+      e.preventDefault();
+
+      const regaloId = document.querySelector("#regalo").value;
+
+      const eventosId = eventos.map((evento) => evento.id);
+
+      if(eventosId.length === 0 || regaloId === ""){
+        Swal.fire({
+          title: "Error",
+          text: "Selecciona al menos un Evento y un Regalo",
+          icon: "error",
+          confirmButtonText: "Ok",
+        });
+        return;
+      }
+
     }
   }
 })();
